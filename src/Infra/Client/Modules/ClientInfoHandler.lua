e414ddc8bdd69ae46a98dad089fe90430e860ba3
Harrison Lewis <therealstratiz@gmail.com>
2025-05-29 04:43:02 -0500

    feat: Expanded device properties & inputType

--- src/Infra/Client/Modules/ClientInfoHandler.lua
+++ src/Infra/Client/Modules/ClientInfoHandler.lua
@@ -38,6 +38,8 @@ local ProductInfoCache = {} :: {[number] : {PriceInRobux : number}}
 local CurrentClientInfoCache = {} :: {[string] : any}
 local FriendCache = {} :: {[number] : boolean}
 local FriendsOnline = 0
+local PendingUpdate = false
+local CurrentInputType = "keyboard" -- Default input type
 
 --= Public Variables =--
 
@@ -46,7 +48,16 @@ local FriendsOnline = 0
 local function UpdateClientInfo(key : string, value : any, force : boolean?)
     if CurrentClientInfoCache[key] ~= value or force then
         CurrentClientInfoCache[key] = value
-        ClientInfoRemote:FireServer(key, value)
+
+        if PendingUpdate then
+            return
+        end
+
+        PendingUpdate = true
+        task.defer(function()
+            PendingUpdate = false
+            ClientInfoRemote:FireServer(CurrentClientInfoCache)
+        end)
     end
 end
 
@@ -74,25 +85,103 @@ local function UpdateFriendCache()
     UpdateClientInfo("friendsOnline", FriendsOnline)
 end
 
+local function DetermineInputTypeString(InputEnum) 
+    if InputEnum == Enum.UserInputType.Keyboard then
+        return "keyboard"
+    elseif InputEnum == Enum.UserInputType.Touch then
+        return "touch"
+    elseif string.match(tostring(InputEnum),"Gamepad") then
+        return "gamepad"
+    end
+end
+
 --= API Functions =--
 
-function ClientInfoHandler:GetDeviceType() : string
+function ClientInfoHandler:GetDeviceType() : (string, string)
+    -- Determine device type
+    local deviceType = "unknown"
     if UserInputService.VREnabled or VRService.VREnabled then
-        return "vr"
+        deviceType = "vr"
     elseif UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled then
-		return "mobile"
+		deviceType = "mobile"
 	elseif UserInputService.GamepadEnabled and not UserInputService.KeyboardEnabled then
-		return "console"
+		deviceType = "console"
 	elseif UserInputService.KeyboardEnabled then
-		return "pc"
-	else
-		return "unknown"
+		deviceType = "pc"
 	end
+
+    -- Determine device subtype
+    local deviceSubType = "unknown"
+    if deviceType == "mobile" then
+        local camera = workspace.CurrentCamera
+        if camera then
+            local longestSide = math.max(camera.ViewportSize.X, camera.ViewportSize.Y)
+            local shortestSide = math.min(camera.ViewportSize.X, camera.ViewportSize.Y)
+            local aspectRatio = longestSide / shortestSide
+            if aspectRatio > 1.8 then
+                deviceSubType = "phone"
+            elseif aspectRatio <= 1.7 then
+                deviceSubType = "tablet"
+            else -- Devices in this range can be either phone or tablet equally. Older devices share the 16:9 aspect ratio.
+                deviceSubType = "phone" -- We'll assume phone for now, since phone users are more common.
+            end
+        end
+    elseif deviceType == "console" then
+        local imageUrl = UserInputService:GetImageForKeyCode(Enum.KeyCode.ButtonX)
+        if string.find(imageUrl, "Xbox") then
+            deviceSubType = "xbox"
+        elseif string.find(imageUrl, "PlayStation") then
+            deviceSubType = "playstation"
+        else
+            deviceSubType = "unknown"
+        end
+    elseif deviceType == "vr" then
+        deviceSubType = "unknown" -- No reliable way to determine VR headset type in Roblox.
+    elseif deviceType == "pc" then
+        deviceSubType = "unknown" -- No reliable way to determine PC type in Roblox
+    end
+
+    return deviceType, deviceSubType
 end
+
+
 
 --= Initializers =--
 function ClientInfoHandler:Init()
-    UpdateClientInfo("device", self:GetDeviceType(), true)
+    local deviceType, deviceSubType = self:GetDeviceType()
+    UpdateClientInfo("deviceSubType", deviceSubType)
+    UpdateClientInfo("device", deviceType, true)
+
+    -- Input type detection
+
+    local function inputTypeChanged(inputType : string)
+        if CurrentInputType ~= inputType then
+            CurrentInputType = inputType
+            UpdateClientInfo("inputType", CurrentInputType)
+        end
+    end
+
+    if UserInputService.GamepadEnabled then
+        inputTypeChanged("gamepad")
+    elseif UserInputService.TouchEnabled then
+        inputTypeChanged("touch")
+    else
+        inputTypeChanged("keyboard")
+    end
+
+    --// Detect CurrentInputType changes from 'LastInputType'
+    UserInputService.LastInputTypeChanged:Connect(function(lastType)
+        local newType = DetermineInputTypeString(lastType)
+        if newType and newType ~= CurrentInputType then
+            inputTypeChanged(newType)
+        end
+    end)
+
+     UserInputService.InputChanged:Connect(function(InputObject)
+        if InputObject.UserInputType == Enum.UserInputType.MouseMovement then
+            inputTypeChanged("keyboard")
+        end
+    end)
 
     -- Friends online
     task.spawn(function()
